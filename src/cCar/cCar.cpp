5c4e189ca1b0885980d3753de650b4072dc399c5
demdecuong <32518096+demdecuong@users.noreply.github.com>
2018-11-22 20:12:44 +0700

    Update cCar.cpp

--- src/cCar/cCar.cpp
+++ src/cCar/cCar.cpp
@@ -1,1 +1,27 @@
 
+#include "cCar.h"
+#include "cConsole.h"
+using namespace std;
+
+void cCar::draw()
+{
+	int x = cCar::cEnemy::getX();
+	int y = cCar::cEnemy::getY();
+	gotoXY(x, y);
+	cout << " ";
+	for (int i = 0; i < 5; i++)
+		cout << "_";
+	cout << " ";
+	gotoXY(x, y + 1);
+	cout << "/    "<<(char)220<<"\\" ;
+	gotoXY(x, y + 2);
+	for (int i = 0; i < 7; i++) {
+		if (i == 0)cout << (char)240;
+		else
+		if (i == 1 || i == 5)cout << "O";
+		else
+			//cout << (char)196;
+			cout << '-';
+	}
+	cout << (char)217;
+}
