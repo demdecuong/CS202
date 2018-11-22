148a5c2f757530a84c8310974b6f2e013bd07d97
demdecuong <32518096+demdecuong@users.noreply.github.com>
2018-11-22 16:00:53 +0700

    Update cMap.cpp

--- src/cMap/cMap.cpp
+++ src/cMap/cMap.cpp
@@ -1,1 +1,23 @@
 #include "cMap.h"
+#include "cConsole.h"
+
+void cMap::printMap()
+{
+	int temp1 = width, temp2 = height;
+	TextColor(14);
+	cout << " " << (char)218;
+	for (int i = 0; i<width; i++)
+		cout << (char)196;
+	cout << (char)191 << endl;
+	while (temp2 > 0) {
+		cout << " " << (char)179;
+		for (int i = 0; i < temp1; i++)
+			cout << " ";
+		cout << (char)179 << endl;
+		temp2--;
+	}
+	cout << " " << (char)192;
+	for (int i = 0; i < temp1; i++)
+		cout << (char)196;
+	cout << (char)217 << endl;
+}
