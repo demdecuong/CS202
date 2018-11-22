43f1ee5f35bf393323767d1f6a77a5e3efb01dd2
demdecuong <32518096+demdecuong@users.noreply.github.com>
2018-11-22 19:53:02 +0700

    Update cTruck.cpp

--- src/cTruck/cTruck.cpp
+++ src/cTruck/cTruck.cpp
@@ -5,17 +5,20 @@ using namespace std;
 
 void cTruck::draw()
 {
+	
 	int x = cTruck::cEnemy::getX();
 	int y = cTruck::cEnemy::getY();
 	gotoXY(x , y);
 	cout << (char)218;
 	for (int i = 0; i < 6; i++)
-		cout << char(196);
+		cout << char(196);	
 	cout << (char)191;
 	
-	cout << endl;
+	//cout << endl;
+	gotoXY(x,y+1);
 	cout << (char)179 << "     [" << (char)179;
-	cout << endl;
+	//cout << endl;
+	gotoXY(x, y+2);
 	cout << (char)192;
 	for (int i = 0; i < 6; i++) {
 		if (i == 0 || i == 5)cout << "O";
@@ -23,4 +26,5 @@ void cTruck::draw()
 			cout << (char)196;
 	}
 	cout << (char)217;
+	//max_x = 8
 }
