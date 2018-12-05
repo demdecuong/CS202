3c9a77d71c5f8097d1c42b0e59a43bef1b5c5bbd
demdecuong <32518096+demdecuong@users.noreply.github.com>
2018-12-05 18:34:21 +0700

    Update cBird.h

--- src/cBird/cBird.h
+++ src/cBird/cBird.h
@@ -1,1 +1,16 @@
 
+#ifndef cBird_h
+#define cBird_h
+#include "cEnemy.h"
+class cBird : public cEnemy
+{
+	char **a; //a[3][6]
+public:
+	void draw();
+	char ** shape();
+	void sound();
+	void test();
+	cBird();
+	~cBird();
+};
+#endif // !cBird_h
