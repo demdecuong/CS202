a7ad3ca2c2147cc714e2c7b4f54c066b74789eed
demdecuong <32518096+demdecuong@users.noreply.github.com>
2018-12-05 18:35:56 +0700

    Update cDinausor.h

--- src/cDinausor/cDinausor.h
+++ src/cDinausor/cDinausor.h
@@ -1,1 +1,15 @@
 
+#ifndef cDino_h
+#define cDino_h
+#include "cEnemy.h"
+class cDinosaur : public cEnemy
+{
+	char **a; //a[3][6]
+public:
+	char ** shape();
+	void sound();
+	void test();
+	cDinosaur();
+	~cDinosaur();
+};
+#endif // !cBird_h
