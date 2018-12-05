81f047a6b6b23bd286320ad3717f8dabd67c2003
demdecuong <32518096+demdecuong@users.noreply.github.com>
2018-12-05 18:37:03 +0700

    Update cTruck.h

--- src/cTruck/cTruck.h
+++ src/cTruck/cTruck.h
@@ -1,12 +1,15 @@
-
 #ifndef cTruck_h
 #define cTruck_h
 #include "cEnemy.h"
 
 class cTruck : public cEnemy
 {
+	char** a; //a[3][8]
 public:
-	void draw();
-  void sound();
+	char **shape();
+	void sound();
+	void test();
+	cTruck();
+	~cTruck();
 };
 #endif 
